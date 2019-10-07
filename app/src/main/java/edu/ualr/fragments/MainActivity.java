160d64d89de331f26fa80a43ab98805253c1e471
irconde <ivanrodriguezconde@gmail.com>
2019-10-07 12:05:21 -0500

    Bug fixed

--- app/src/main/java/edu/ualr/fragments/MainActivity.java
+++ app/src/main/java/edu/ualr/fragments/MainActivity.java
@@ -3,14 +3,39 @@ package edu.ualr.fragments;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-// TODO 01. We create a new XML file to define the layout of the fragment
-// TODO 02. We define the corresponding "controller" by creating a Fragment java class
+import com.google.android.material.button.MaterialButton;
+
+import edu.ualr.fragments.fragments.FirstFragment;
+
+// TODO 05. In the activity we have to implement the OnItemSelectedListener listener
+
+// TODO 08. The activity needs to make the fragment perform a clear action when the user taps the "clear all" button
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textList;
+    private MaterialButton clearButton;
+    private FirstFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textList = findViewById(R.id.textList);
+        clearButton = findViewById(R.id.clearButton);
+        // TODO 10. In the activity we get access to the fragment using the fragment manager.
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClearButtonClicked();
+            }
+        });
     }
+
+    // TODO 11. We invoke the corresponding method
+
+    // TODO 06. Define the action to take in the activity when the fragment event fires
 }
--- app/src/main/java/edu/ualr/fragments/fragments/FirstFragment.java
+++ app/src/main/java/edu/ualr/fragments/fragments/FirstFragment.java
@@ -1,10 +1,12 @@
 package edu.ualr.fragments.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,10 +18,16 @@ import edu.ualr.fragments.R;
 /**
  * Created by irconde on 2019-10-07.
  */
+
+// TODO 01. We create a interface to communicate events from the fragment to the activity.
+
 public class FirstFragment extends Fragment {
 
-    private Button button;
-    private TextView label;
+    private Button addTextBtn;
+    private EditText editText;
+    private TextView textCountLabel;
+
+    // TODO 03. Define the listener using the interface type defined
 
     @Nullable
     @Override
@@ -29,11 +37,33 @@ public class FirstFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        button.findViewById(R.id.button);
-        label = view.findViewById(R.id.textLabel);
-        button.setOnClickListener(new View.OnClickListener() {
+        addTextBtn = view.findViewById(R.id.addButton);
+        addTextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {}
+            public void onClick(View view) {
+                onAddButtonClicked();
+            }
         });
+        editText = view.findViewById(R.id.inputText);
+        textCountLabel = view.findViewById(R.id.number_texts_added);
     }
+
+    // TODO 07. Now we can fire the event when the user clicks the "Add text" button
+
+    private void updateCounter() {
+        int textCount = Integer.parseInt(this.textCountLabel.getText().toString());
+        this.textCountLabel.setText(String.valueOf(++textCount));
+    }
+
+    // TODO 04. Store the listener (activity) that will have events fired once the fragment is attached
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    // TODO 09. We can just add a new method to the fragment class
+
+    // TODO 02. We define the listener interface as an inner type of the Fragment class
+    // We define the events that the fragment will use to communicate
+
 }
