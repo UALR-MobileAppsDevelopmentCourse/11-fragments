bc0825191f3de6b6b1e0e90f0f2f1b9514d46012
irconde <ivanrodriguezconde@gmail.com>
2019-10-07 12:05:21 -0500

    Starter code

--- app/src/main/java/edu/ualr/fragments/MainActivity.java
+++ app/src/main/java/edu/ualr/fragments/MainActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-// TODO 01. We create a new XML file to define the layout of the fragment
-// TODO 02. We define the corresponding "controller" by creating a Fragment java class
+// TODO 01. We create a new XML file to define the layout of the fragment: first_fragment.xml
+// Simple layout with a TextView and a Button
+// TODO 02. We define the corresponding "controller" by creating a Fragment java class: FirstFragment.java
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TODO 04. Finding fragment by id
     }
 }
