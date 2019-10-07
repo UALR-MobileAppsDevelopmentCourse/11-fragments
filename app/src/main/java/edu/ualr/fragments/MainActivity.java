55a54fb43c92dd06a7c90335a605b62b4af3b9c5
irconde <ivanrodriguezconde@gmail.com>
2019-10-07 12:05:21 -0500

    Starter code

--- app/src/main/java/edu/ualr/fragments/MainActivity.java
+++ app/src/main/java/edu/ualr/fragments/MainActivity.java
@@ -1,16 +1,30 @@
 package edu.ualr.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
-// TODO 01. We create a new XML file to define the layout of the fragment
-// TODO 02. We define the corresponding "controller" by creating a Fragment java class
+import edu.ualr.fragments.fragments.FirstFragment;
+
+// TODO 01. We create a new XML file to define the layout of the fragment [Already done in previous branches]
+// TODO 02. We define the corresponding "controller" by creating a Fragment java class [Already done in previous branches]
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FRAGMENT_TAG = "FirstFragment";
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TODO 07. Load the fragment dynamically
+        FirstFragment fragment = null;
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.fragment_placeholder, fragment, FRAGMENT_TAG);
+        // or ft.add(R.id.your_placeholder, new FooFragment());
+        // Complete the changes added above
+        ft.commit();
     }
 }
--- app/src/main/java/edu/ualr/fragments/fragments/FirstFragment.java
+++ app/src/main/java/edu/ualr/fragments/fragments/FirstFragment.java
@@ -1,6 +1,7 @@
 package edu.ualr.fragments.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +19,23 @@ import edu.ualr.fragments.R;
  */
 public class FirstFragment extends Fragment {
 
+    private static final String INT_VALUE_KEY = "IntValue";
+    private static final String TITLE_VALUE_KEY = "TitleValue";
+    private static final String TAG = FirstFragment.class.getSimpleName();
+
     private Button button;
     private TextView label;
+
+    // TODO 04. We define a static method to create a new instance of the fragment
+    //  and use the setArguments method to set certain arguments into the fragment for later access
+
+
+    // TODO 05. Access the arguments of the fragment within onCreate
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
@@ -29,11 +45,9 @@ public class FirstFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        button.findViewById(R.id.button);
+        button = view.findViewById(R.id.button);
         label = view.findViewById(R.id.textLabel);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {}
-        });
+        // TODO 06. Access the arguments of the fragment within onViewCreated to populate widgets
+        //  with the data in the arguments
     }
 }
