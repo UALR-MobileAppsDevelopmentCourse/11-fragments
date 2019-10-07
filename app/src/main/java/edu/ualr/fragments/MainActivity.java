a5b3ecad7e812bf70db1cef0677796cdae079487
irconde <ivanrodriguezconde@gmail.com>
2019-10-07 13:43:41 -0500

    Fragment dynamically embedded in activity

--- app/src/main/java/edu/ualr/fragments/MainActivity.java
+++ app/src/main/java/edu/ualr/fragments/MainActivity.java
@@ -1,8 +1,10 @@
 package edu.ualr.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+
+import edu.ualr.fragments.fragments.FirstFragment;
 
 // TODO 01. We create a new XML file to define the layout of the fragment
 // TODO 02. We define the corresponding "controller" by creating a Fragment java class
@@ -12,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TODO 04. Add dynamically a new fragment to the activity
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.fragment_placeholder, new FirstFragment());
+        // or ft.add(R.id.your_placeholder, new FooFragment());
+        // Complete the changes added above
+        ft.commit();
     }
 }
