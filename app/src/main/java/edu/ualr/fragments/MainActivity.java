932e8a31079a51d9fe76969656ab93b14fbbe803
irconde <ivanrodriguezconde@gmail.com>
2019-10-07 12:05:21 -0500

    Starter code

--- app/src/main/java/edu/ualr/fragments/MainActivity.java
+++ app/src/main/java/edu/ualr/fragments/MainActivity.java
@@ -1,16 +1,33 @@
 package edu.ualr.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
-// TODO 01. We create a new XML file to define the layout of the fragment
-// TODO 02. We define the corresponding "controller" by creating a Fragment java class
+import edu.ualr.fragments.fragments.FormFragment;
+import edu.ualr.fragments.fragments.ProfileFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    FormFragment formFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        formFragment = new FormFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.fragment_placeholder, formFragment);
+        ft.commit();
+    }
+
+    public void onSaveDataButtonClicked(View view) {
+        formFragment.saveProfileData();
+        view.setVisibility(View.GONE);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_placeholder, new ProfileFragment());
+        ft.commit();
     }
 }
