20e4c5a4f57bd6cb470362381cdfe05e9658c36f
irconde <ivanrodriguezconde@gmail.com>
2019-10-08 09:25:25 -0500

    Starter code

--- app/src/main/java/edu/ualr/fragments/MainActivity.java
+++ app/src/main/java/edu/ualr/fragments/MainActivity.java
@@ -5,16 +5,11 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-// TODO 01. We create a new XML file to define the layout of the fragment
-// TODO 02. We define the corresponding "controller" by creating a Fragment java class
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // TODO 04. Finding fragment by id
-        Fragment demoFragment = getSupportFragmentManager().findFragmentById(R.id.demoFragment);
-
     }
 }
--- app/src/main/java/edu/ualr/fragments/fragments/FirstFragment.java
+++ app/src/main/java/edu/ualr/fragments/fragments/FirstFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,9 +16,6 @@ import edu.ualr.fragments.R;
  */
 public class FirstFragment extends Fragment {
 
-    private Button button;
-    private TextView label;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,11 +24,5 @@ public class FirstFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        button = view.findViewById(R.id.button);
-        label = view.findViewById(R.id.textLabel);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {}
-        });
     }
 }
