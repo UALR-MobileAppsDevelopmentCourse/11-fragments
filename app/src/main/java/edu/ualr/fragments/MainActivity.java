297510d70d40188e450c9e24b676585ad9b30791
irconde <ivanrodriguezconde@gmail.com>
2019-10-28 12:13:35 -0500

    Modified TODO comment

--- app/src/main/java/edu/ualr/fragments/MainActivity.java
+++ app/src/main/java/edu/ualr/fragments/MainActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 
 // TODO 01. We create a new XML file to define the layout of the fragment: first_fragment.xml
 // Simple layout with a TextView and a Button
-// TODO 02. We define the corresponding "controller" by creating a Fragment java class: FirstFragment.java
+// TODO 02. We define the corresponding "controller" by creating a Fragment java class
+// TODO 02.01. We define one member per element in the layout
+// TODO 02.02. We override the OnCreate method to inflate the layout of the fragment
+// TODO 02.03. We override the onViewCreated method to get a reference to the elements in the corresponding layout
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // TODO 04. Finding fragment by id
+        // TODO 04. Getting a reference of the fragment by using the findFragmentById method from the SupportFragmentManager
     }
 }
